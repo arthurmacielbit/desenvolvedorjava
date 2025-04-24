f3a536546d512c6dfc1481e3bfb096b6e5bf6b0a
ead <ead@academico.local>
2025-04-24 09:35:22 -0300

    aprendendo git

---
+++ jtransfer/aula1/src/App.java
@@ -0,0 +1,59 @@
+
+/* testando código */
+// import java.io.FileWriter;
+// import java.io.IOException;
+// import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // declaração de variáveis
+        String nome;// referência 
+        int opcaoTransporte;// camelCasting
+        String meioTransporte = "";
+
+        // classe para teclado
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("==Pesquisa de Transporte Diário");
+        System.out.println("Digite seu nome");
+        nome = scanner.nextLine();
+        System.out.println(nome);
+        System.out.println("Como você vai ao trabalho?");
+    System.out.println("1-Metrô");
+    System.out.println("2-Ônibus");
+    System.out.println("3-Caminhada");
+    System.out.println("4-Uber");
+    System.out.println("5-Táxi");
+    System.out.println("6-Carro");
+    System.out.println("Escolha uma opção (1-6)");
+ 
+    //exercício
+    //faça como eu fiz
+    //leia a opcaoTransporte e mostre a opção lida
+    // casting (conversão) - texto/número
+opcaoTransporte = scanner.nextInt();
+
+switch(opcaoTransporte){
+    case 1:
+    meioTransporte = "Metrô";break;
+    case 2:
+    meioTransporte = "Ônibus";break;
+    case 3:
+    meioTransporte = "Caminhada";break;
+    case 4:
+    meioTransporte = "Uber";break;
+    case 5:
+    meioTransporte = "Táxi";break;
+    case 6:
+    meioTransporte = "Carro";break; 
+
+}
+//task de amanhã: salvar os dados no arquivo. 
+//Task no GitHub (Git e Github)
+System.out.println("Nome:"+nome+","+"Meio do Transporte: "+meioTransporte);
+
+
+    }
+}
