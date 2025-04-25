22f75f5c25564c596dbe1fbd0a0bf82f32b8ee89
arthurmacielbit <ead@academico.local>
2025-04-25 11:46:23 -0300

    parte sem a primeira linha de scanner funcionando porem com o resto do código

--- jtransfer/aula1/src/AppEscola.java
+++ jtransfer/aula1/src/AppEscola.java
@@ -6,6 +6,13 @@
   //main = principal
   //String args[] = parâmetro inicial
   public static void main(String args[]){
+  Scanner teclado = new Scanner(System.in);
+  System.out.println("Qual o seu nome");
+  String nome = teclado.nextLine();
+  System.out.println("Qual a sua idade");
+  int idade = teclado.nextInt();
+  System.out.println("Tem Faculdade: 0-não e 1-sim");
+  boolean temFaculdade = teclado.nextBoolean();
   //método principal
   //Classe objeto = new Constructor(parametros);
   Aluno aluno = new Aluno("Arthur",19,true);
